eb0cda35c8b380bc64c1ff4527272175ecfa409d
hotlib <jackiechanaaa@gmail.com>
2013-10-12 14:01:18 -0700

    parsing date (with absolute coordinates of elements..meh..)

--- svnparse.hs
+++ svnparse.hs
@@ -5,23 +5,35 @@ import Text.ParserCombinators.Parsec hiding (try)
 import Control.Applicative hiding (many, (<|>))
 import Control.Monad
 
-data SvnEntry = SvnEntry { getMessage :: String, getId::Int, getName :: String, getDate :: String, getSvnActions::[SvnAction] } deriving( Show ) 
+data SvnEntry = SvnEntry { getMessage :: String, getId::Int, getName :: String, getDate :: SvnDate, getSvnActions::[SvnAction] } deriving( Show ) 
 data SvnAction = SvnAction { getAction :: Char, getPath :: String} deriving( Show ) 
-
+data SvnDate = SvnDate {getYear :: Integer , getMonth :: Integer, getDay :: Integer,  getHour :: Integer, getMinute :: Integer, getDayName :: String   } deriving( Show ) 
 
 
 svnChanges :: Parser SvnAction
 svnChanges =  liftA2 SvnAction (spaces >> anyChar) (spaces >> (many $ noneOf "\n\r"))
 				
+svnDate :: Parser SvnDate
+svnDate = SvnDate
+   <$> (count 4 digit >>= toDigit)  
+   <*> (anyChar >> count 2 digit >>= toDigit)    
+   <*> (anyChar >> count 2 digit >>= toDigit)   
+   <*> (spaces >> count 2 digit >>= toDigit) 
+   <*> (anyChar >> count 2 digit >>= toDigit)   
+   <*> (count 11 anyChar >> count 3 letter) 
+   <* count 14 anyChar
+   where
+   	toDigit = return . read
+	
 
 svnEntry ::  Parser SvnEntry
 svnEntry = SvnEntry 
 	<$> (manyTill anyChar (try brakeline)) 
 	<*> (many (oneOf "-\n\rr") >> many1 digit >>= (\n -> return (read n))) 
 	<*> (separator >> many1 alphaNum) 
-	<*> (separator >> manyTill anyChar (try separator)) 		
+	<*> (separator >> svnDate)		
  	<*> (skipSomePart >> manyTill svnChanges ( choice [try newnewline, try myEof]))   
- 	where 
+ 	where
  		separator = string " | "
  		newnewline = string "\r\n\r\n"
  		skipSomePart = manyTill anyChar (try (string "  "))
@@ -32,8 +44,6 @@ main :: IO ()
 main = do
 	 	
 	 	s <- readFile "svn_test.log"
-	 	let a = parse (count 3 svnEntry) "test" s
-	 	let b = parse (many svnEntry) "test" s
-	 	print b
-		-- print $ length <$> b
+	 	let a = parse (many svnEntry) "test" s
+	 	print a
 	 	
