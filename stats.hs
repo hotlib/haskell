dd0392b797b7d4c07d2400570a5d6a957016aa74
hotlib <jackiechanaaa@gmail.com>
2013-10-17 03:39:14 -0700

    adding command line arguments

--- stats.hs
+++ stats.hs
@@ -3,6 +3,8 @@
 import Database.PostgreSQL.Simple
 import Data.Time.Clock (UTCTime)
 import Text.PrettyPrint.ANSI.Leijen
+import System.Console.GetOpt
+import System.Environment
 
 type TableSizeAndWritesSelectResult = (String, String, Maybe Int)
 type LeastUsedIndexesResult = (String, Int, Int)
@@ -203,13 +205,9 @@ tableSizeAndWritesSelect = "SELECT \
 		\ORDER BY write_percentage DESC \
 		\limit 5"
 
-myConnection :: ConnectInfo
-myConnection = defaultConnectInfo {connectHost = "xxx", connectPort = 5432,
-					 connectUser = "xxx", connectPassword = "xxx", connectDatabase = "xxx"}
-
-pgRunQuery :: (FromRow r, Show r) =>  Query -> IO [r]
-pgRunQuery queryString = do 
-	conn <- connect myConnection
+pgRunQuery :: (FromRow r, Show r) => ConnectInfo -> Query -> IO [r]
+pgRunQuery connection queryString = do 
+	conn <- connect connection
   	query_ conn queryString 
 
 pgPrintResult :: (FromRow r, Show r) => IO [r] -> IO ()
@@ -220,37 +218,79 @@ pgPrintResult query  = do
 
 pgPutDoc :: String -> Doc -> IO ()
 pgPutDoc caption doc = putDoc $ (text caption) <+> dullred doc <> linebreak 
-				
+		
+header progName = "Usage: " ++ progName ++ " [OPTION...]"
+
+data PgArguments = Ip String | DbName String | Username String | Password String
+
+options :: [OptDescr PgArguments]
+options = [
+	Option ['i'] ["ip"]       (ReqArg (\s -> Ip s) "IP_ADDRESS") "IP Address of the PostgreSQL server",
+	Option ['d'] ["db"] (ReqArg (\s -> DbName s) "DB_NAME") "Name of Database",
+	Option ['u'] ["user"] (ReqArg (\s -> Username s) "USERNAME") "Username for authentication",
+	Option ['p'] ["password"] (ReqArg (\s -> Password s) "PASSWORD") "Password for authentication"
+	]
+
+modifyConnection :: ConnectInfo -> PgArguments -> ConnectInfo
+modifyConnection connection (Ip ip) = connection {connectHost = ip, connectPort = 5432} 
+modifyConnection connection (DbName name) = connection {connectDatabase = name}
+modifyConnection connection  (Username user) = connection {connectUser = user}
+modifyConnection connection (Password password) = connection {connectPassword = password}
+
+evalArgs :: IO ( Maybe ConnectInfo )
+evalArgs = do
+	args <- getArgs
+	progName <- getProgName
+	let a = getOpt RequireOrder options args
+	case a of
+		([], _, _) -> (error $ printUsage progName) >> return Nothing
+		(flags, [], []) -> return . Just $ createConnection flags
+	 	(_, nonOpts, []) -> (error $ unrecognizedArgs nonOpts) >> return Nothing
+	 	(_, _, msgs) -> (error $ concat msgs ++ printUsage progName) >> return Nothing
+	 where
+	 	printUsage p = (usageInfo (header p) options)
+	 	createConnection xs = foldl (modifyConnection) defaultConnectInfo xs
+	 	unrecognizedArgs nonOpts = "unrecognized arguments: " ++ unwords nonOpts
+
+
+runScripts :: ConnectInfo -> IO ()
+runScripts c = do
+	pgPutDoc "TOP TABLES BY SIZE" biggestTableText
+	pgPrintResult ((pgRunQuery c biggestTableSelect) :: IO [BiggestTableResult])
+
+	pgPutDoc "TOP ACCESSED TABLES" mostAccessedTableText
+	pgPrintResult ((pgRunQuery c mostAccessedTableSelect) :: IO [MostAccessedTableResult])
+	
+	pgPutDoc "TABLES WITH WORST BUFFER HIT AVERAGE" sharredBufferTableText
+	pgPrintResult ((pgRunQuery c sharredBufferTableSelect) :: IO [SharredBufferTableResult])
+
+	pgPutDoc "BIGGEST TABLES WHERE INDEX SCANS ARE LESS THAN 80%" leastUsedIndexesText   
+	pgPrintResult ((pgRunQuery c leastUsedIndexesSelect) :: IO [LeastUsedIndexesResult])
+		
+	pgPutDoc "TOP WRITE-INTENSIVE TABLES" tableSizeAndWritesText   
+	pgPrintResult ((pgRunQuery c tableSizeAndWritesSelect) :: IO [TableSizeAndWritesSelectResult])
+
+	pgPutDoc "AVG. BUFFER HIT RATIO OVER DATABASE" sharredBufferOverallText 
+	pgPrintResult ((pgRunQuery c sharredBufferOverallSelect) :: IO [SharredBufferOverallResult])
+		
+	pgPutDoc "LEAST ACCESSED INDEXES IN DATABASE" unusedIndexesText 
+	pgPrintResult ((pgRunQuery c unusedIndexesSelect) :: IO [UnusedIndexesResult])
+		
+	pgPutDoc "POTENTIALLY STUCK TRANSACTIONS" transactionStuckText 
+	pgPrintResult ((pgRunQuery c transactionStuckSelect) :: IO [TransactionStuckResult])
+
+	pgPutDoc "LONGEST RUNNING TRANSACTION IN SECONDS" findLongestTransactionText 
+	pgPrintResult ((pgRunQuery c findLongestTransactionSelect) :: IO [FindLongestTransactionResult])
+
+	pgPutDoc "LONGEST RUNNING QUERIES" findLongestQueryText 
+	pgPrintResult ((pgRunQuery c findLongestQuerySelect) :: IO [FindLongestQueryResult])
+	
+	pgPutDoc "UNVACUUMED OR UNANALYZED TABLES (OLDER THAN 14 DAYS)" lastAnalyzedAndVacuumedText
+	pgPrintResult ((pgRunQuery c lastAnalyzedAndVacuumedSelect) :: IO [LastAnalyzedAndVacuumedResult])
+
 main = do
-		pgPutDoc "TOP TABLES BY SIZE" biggestTableText
-		pgPrintResult ((pgRunQuery biggestTableSelect) :: IO [BiggestTableResult])
+	c <- evalArgs
+	case c of
+		(Just connection) -> runScripts connection
+		Nothing -> return ()
 
-		pgPutDoc "TOP ACCESSED TABLES" mostAccessedTableText
-		pgPrintResult ((pgRunQuery mostAccessedTableSelect) :: IO [MostAccessedTableResult])
-	
-		pgPutDoc "TABLES WITH WORST BUFFER HIT AVERAGE" sharredBufferTableText
-		pgPrintResult ((pgRunQuery sharredBufferTableSelect) :: IO [SharredBufferTableResult])
-
-		pgPutDoc "BIGGEST TABLES WHERE INDEX SCANS ARE LESS THAN 80%" leastUsedIndexesText   
-		pgPrintResult ((pgRunQuery leastUsedIndexesSelect) :: IO [LeastUsedIndexesResult])
-		
-		pgPutDoc "TOP WRITE-INTENSIVE TABLES" tableSizeAndWritesText   
-		pgPrintResult ((pgRunQuery tableSizeAndWritesSelect) :: IO [TableSizeAndWritesSelectResult])
-
-		pgPutDoc "AVG. BUFFER HIT RATIO OVER DATABASE" sharredBufferOverallText 
-		pgPrintResult ((pgRunQuery sharredBufferOverallSelect) :: IO [SharredBufferOverallResult])
-		
-		pgPutDoc "LEAST ACCESSED INDEXES IN DATABASE" unusedIndexesText 
-		pgPrintResult ((pgRunQuery unusedIndexesSelect) :: IO [UnusedIndexesResult])
-		
-		pgPutDoc "POTENTIALLY STUCK TRANSACTIONS" transactionStuckText 
-		pgPrintResult ((pgRunQuery transactionStuckSelect) :: IO [TransactionStuckResult])
-
-		pgPutDoc "LONGEST RUNNING TRANSACTION IN SECONDS" findLongestTransactionText 
-		pgPrintResult ((pgRunQuery findLongestTransactionSelect) :: IO [FindLongestTransactionResult])
-
-		pgPutDoc "LONGEST RUNNING QUERIES" findLongestQueryText 
-		pgPrintResult ((pgRunQuery findLongestQuerySelect) :: IO [FindLongestQueryResult])
-	
-		pgPutDoc "UNVACUUMED OR UNANALYZED TABLES (OLDER THAN 14 DAYS)" lastAnalyzedAndVacuumedText
-		pgPrintResult ((pgRunQuery lastAnalyzedAndVacuumedSelect) :: IO [LastAnalyzedAndVacuumedResult])
